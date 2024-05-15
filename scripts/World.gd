589f9554e55cfcedd160f9b90572a18dda1271ae
ghartemann <g.hartemann@gmail.com>
2024-05-15 13:45:39 +0200

    fx

--- scripts/World.gd
+++ scripts/World.gd
@@ -9,11 +9,12 @@ var building = "res://scenes/components/building.tscn"
 @onready var camera = %Camera3D
 
 @export var selector: Node3D # The 'cursor'
-@export var selector_container: Node3D # Node that holds a preview of the structure
+
 
 func _ready():
 	map = DataMap.new()
 	plane = Plane(Vector3.UP, Vector3.ZERO)
+	$AudioStreamPlayer.play()
 
 func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
@@ -22,21 +23,16 @@ func _process(delta):
 		camera.project_ray_normal(mouse_pos)
 	)
 	
-	print(world_position)
-	
 	var x_adjusted = round(world_position.x / 100)
 	var z_adjusted = round(world_position.z / 100)
 	
-	var gridmap_position = Vector3(x_adjusted, 0.1, z_adjusted)
+	var gridmap_position = Vector3(x_adjusted, 0.001, z_adjusted)
 	selector.position = lerp(selector.position, gridmap_position, delta * 40)
-
-func _on_clicko():
-	print('clocko')
 
 
 func _on_main_clicko():
 	var building_res = load(building)
 	var build = building_res.instantiate()
 	
-	build.position = selector.position
+	build.position = Vector3(selector.position.x, 0.25, selector.position.z)
 	get_node("/root/Main/World/GridMap").add_child(build)
